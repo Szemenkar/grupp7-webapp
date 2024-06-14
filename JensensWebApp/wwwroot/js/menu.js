3a70cb78b3982629ac7898c57814e5499c148042
Clone888 <dwclone@gmail.com>
2024-06-14 15:05:41 +0200

    Javascript uncomment

--- JensensWebApp/wwwroot/js/menu.js
+++ JensensWebApp/wwwroot/js/menu.js
@@ -1,58 +1,16 @@
-// // const menu = document.querySelector('.menu');
-// // const headerHeight = document.querySelector('header').offsetHeight;
-// // let prevScrollPos = window.scrollY;
-
-// // window.addEventListener('scroll', () => {
-// //   const currentScrollPos = window.scrollY;
-// //   if (prevScrollPos > currentScrollPos) {
-// //     // Användaren scrollar uppåt
-// //     menu.style.top = '0';
-// //     menu.style.opacity = '1';
-// //   } else {
-// //     // Användaren scrollar nedåt
-// //     menu.style.top = '-100%';
-// //     menu.style.opacity = '0';
-// //   }
-// //   prevScrollPos = currentScrollPos;
-// // });
-
-// // window.addEventListener('load', () => {
-// //   // När sidan laddas första gången
-// //   menu.style.top = `${headerHeight}px`;
-// // });
-
-// // window.addEventListener('scroll', () => {
-// //   const currentScrollPos = window.scrollY;
-// //   if (currentScrollPos > headerHeight) {
-// //     // Användaren har scrollat förbi headern
-// //     menu.style.position = 'fixed';
-// //     menu.style.top = '0';
-// //   } else {
-// //     // Användaren är fortfarande över headern
-// //     menu.style.position = 'absolute';
-// //     menu.style.top = `${headerHeight}px`;
-// //   }
-// // });
-
-
-
-
-
 // const menu = document.querySelector('.menu');
 // const headerHeight = document.querySelector('header').offsetHeight;
 // let prevScrollPos = window.scrollY;
 
 // window.addEventListener('scroll', () => {
 //   const currentScrollPos = window.scrollY;
-//   if (currentScrollPos > headerHeight) {
-//     // Användaren har scrollat förbi headern
-//     menu.style.position = 'fixed';
+//   if (prevScrollPos > currentScrollPos) {
+//     // Användaren scrollar uppåt
 //     menu.style.top = '0';
 //     menu.style.opacity = '1';
 //   } else {
-//     // Användaren är fortfarande över headern
-//     menu.style.position = 'absolute';
-//     menu.style.top = `${headerHeight}px`;
+//     // Användaren scrollar nedåt
+//     menu.style.top = '-100%';
 //     menu.style.opacity = '0';
 //   }
 //   prevScrollPos = currentScrollPos;
@@ -62,3 +20,45 @@
 //   // När sidan laddas första gången
 //   menu.style.top = `${headerHeight}px`;
 // });
+
+// window.addEventListener('scroll', () => {
+//   const currentScrollPos = window.scrollY;
+//   if (currentScrollPos > headerHeight) {
+//     // Användaren har scrollat förbi headern
+//     menu.style.position = 'fixed';
+//     menu.style.top = '0';
+//   } else {
+//     // Användaren är fortfarande över headern
+//     menu.style.position = 'absolute';
+//     menu.style.top = `${headerHeight}px`;
+//   }
+// });
+
+
+
+
+
+const menu = document.querySelector('.menu');
+const headerHeight = document.querySelector('header').offsetHeight;
+let prevScrollPos = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.scrollY;
+  if (currentScrollPos > headerHeight) {
+    // Användaren har scrollat förbi headern
+    menu.style.position = 'fixed';
+    menu.style.top = '0';
+    menu.style.opacity = '1';
+  } else {
+    // Användaren är fortfarande över headern
+    menu.style.position = 'absolute';
+    menu.style.top = `${headerHeight}px`;
+    menu.style.opacity = '0';
+  }
+  prevScrollPos = currentScrollPos;
+});
+
+window.addEventListener('load', () => {
+  // När sidan laddas första gången
+  menu.style.top = `${headerHeight}px`;
+});
